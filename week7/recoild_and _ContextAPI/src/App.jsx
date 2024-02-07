7c9233d22af02be556ae9c3558c3ee6c284ce189
rajat bansal <rajatbansal28082003@gmail.com>
2024-02-08 04:52:17 +0530

    recoil

--- week7/recoild_and _ContextAPI/src/App.jsx
+++ week7/recoild_and _ContextAPI/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("app rerender")
+  return (
+    <>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </>
+  );
+}
+function Count() {
+  console.log("count rerender")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CountRender />
+      <Buttons />
+      <StateEvenOrOdd />
     </>
+  );
+}
+function CountRender() {
+  console.log("countrenderer rerender")
+
+  const count = useRecoilValue(countAtom);
+  return <>{count}</>;
+}
+function Buttons() {
+  console.log("button rerender")
+
+  const  setCount = useSetRecoilState(countAtom);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount(count =>count+1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count => count-1);
+        }}
+      >
+        Decrement
+      </button>
+    </>
+  );
+}
+function StateEvenOrOdd(){
+  const count = useRecoilValue(evenSelector);
+  return(
+    <div>
+      {count ? <>is odd </>:<>is even</>}
+    </div> 
   )
 }
 
-export default App
+export default App;
